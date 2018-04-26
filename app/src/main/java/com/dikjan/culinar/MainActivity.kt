fc5c5dc8453e4786d8158f9cfae51c15c45bcffe
bojan <dream.muhtadiin@gmail.com>
2018-04-26 23:38:53 +0700

    create order init menu

--- app/src/main/java/com/dikjan/culinar/MainActivity.kt
+++ app/src/main/java/com/dikjan/culinar/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.dikjan.culinar
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.dikjan.culinar.ordering.OrderingActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnOrder = findViewById(R.id.btnOrder) as Button
+        btnOrder.setOnClickListener {
+            startActivity(Intent(this@MainActivity,OrderingActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/dikjan/culinar/ordering/OrderingActivity.kt
@@ -0,0 +1,13 @@
+package com.dikjan.culinar.ordering
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.dikjan.culinar.R
+
+class OrderingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ordering_main)
+    }
+}
